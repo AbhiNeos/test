79c9548f0569748b073ad3fb6fae6711ab941c32
AbhiNeos <125634679+AbhiNeos@users.noreply.github.com>
2026-07-24 15:34:54 +0530

    Delete wordpress directory

--- wordpress/sync-theme.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Syncing Fashion Shop theme to WordPress container...
-docker cp "%~dp0fashion-shop" fashion_shop_wp:/var/www/html/wp-content/themes/fashion-shop
-echo Done! Theme updated.
-pause
